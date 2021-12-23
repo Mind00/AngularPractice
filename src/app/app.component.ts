64590f33c97d4b724dc41840200f53e75f071340
Mind00 <paudeluttam69@gmail.com>
2021-12-23 21:14:53 +0545

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  reactiveForm : FormGroup
+ngOnInit(): void {
+this.reactiveForm = new FormGroup({
+  'userBasic': new FormGroup({
+    'firstname':  new FormControl('', {validators:[Validators.required]}),
+    'lastname': new FormControl('null', [Validators.required]),
+    'address': new FormControl('', Validators.required)
+  }),
+  'gender': new FormControl('Male', Validators.required),
+  'state': new FormControl('', Validators.required)
+})
+
+}
   name1: string = 'I love Angular';
 
   states = [
@@ -24,9 +39,6 @@ export class AppComponent {
   ];
 
   onAdd(){
-    this.states.push({
-      id:4,
-      sname: 'lalitpur'
-    });
+    
   }
 }
